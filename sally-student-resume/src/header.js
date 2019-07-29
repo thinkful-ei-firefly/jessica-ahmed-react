c91c647049195de366ff696416b7a0610a7f4186
Jess Doyle <jessica.e.doyle@gmail.com>
2019-07-29 11:55:30 -0400

    add header HTML to app

---
+++ sally-student-resume/src/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function Header () {
+    return (
+    <header>
+    <h1>Sally Student</h1>
+    <p><em>Computer <strong><a href="https://en.wikipedia.org/wiki/Programmer">Programmer</a></strong></em></p>
+    <p>I'm an aspiring full stack web developer. I love cats.</p>
+    </header>
+    );
+}
+
+export default Header;
